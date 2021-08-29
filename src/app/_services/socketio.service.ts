7eb8ab75dde30bbea1658412a61d53f6fae315fc
bertman12 <eddydaniel94@gmail.com>
2021-08-29 01:42:58 -0700

    implemented bones from component framework

--- src/app/_services/socketio.service.ts
+++ src/app/_services/socketio.service.ts
@@ -15,12 +15,31 @@ export class SocketioService {
     this.socket = io("http://localhost:3000");
     console.log('Creating connection');
     console.log('Socket id:', this.socket.id);
+    console.log(this.socket.id);
+  
   }
 
   pingServer(){
     const msg:string = "Pinging server now!"
     this.socket.emit('myEvent', msg);
   }
+
+  joinRoom(){
+
+  }
+
+  leaveRoom(){
+
+  }
+
+  joinServer(){
+
+  }
+
+  leaveServer(){
+
+  }
+
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocketioService } from './_services/socketio.service';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MapComponent } from './map/map.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { MessageComponent } from './chat-room/message/message.component';
+import { MessageHeaderComponent } from './chat-room/message/message-header/message-header.component';
+import { MessageListComponent } from './chat-room/message-list/message-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    UserProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    MapComponent,
+    ChatRoomComponent,
+    MessageComponent,
+    MessageHeaderComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule,
