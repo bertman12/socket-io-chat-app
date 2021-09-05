f39c2622ca12d87d6f87408833dca4374fd6f4f8
bertman12 <eddydaniel94@gmail.com>
2021-09-05 12:46:06 -0700

    bootstrap wrestling championship complete. read bs docs and restyled navbar to be resposive. need to add more components for server and room searching

---
+++ src/app/navbar/server-room-dropdown/server-room-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-room-dropdown',
+  templateUrl: './server-room-dropdown.component.html',
+  styleUrls: ['./server-room-dropdown.component.css']
+})
+export class ServerRoomDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
