1f59ed182912a5b0a9d53d08e07e3ef4fd43c5b8
bertman12 <eddydaniel94@gmail.com>
2021-09-05 20:09:21 -0700

    restyled navbar, added functionality with dropdowns. made responsive styling

--- src/app/navbar/server-room-dropdown/server-room-dropdown.component.ts
+++ src/app/navbar/server-room-dropdown/server-room-dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-room-dropdown',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-room-dropdown.component.css']
 })
 export class ServerRoomDropdownComponent implements OnInit {
-
+  // type is server or room
+  @Input() dropdownType: string = 'server';
+  
+  locArr:string[] = [
+    'Test1',
+    'Test2',
+    'Test3',
+  ]
+  title:string = ''
+  itemSelected: string='';
   constructor() { }
 
   ngOnInit(): void {
+    if(this.dropdownType === 'server') this.title ="Server";
+    else this.title = "Room"
+  }
+
+  onItemClick(name:string){
+    this.itemSelected = name;
   }
 
 }
